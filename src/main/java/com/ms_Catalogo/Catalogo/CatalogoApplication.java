c9b932a2cedf98a621cdec787889f3df3e67bc41
devroa <roa.alejandra02@gmail.com>
2026-06-14 11:46:35 -0400

    se le agrego la anotacion @EnableFeignClients ya que la relacion con inventario es bidereccional y en este caso es mejor utilizar Feign para la comunicacion.En el caso de subida de archivos se tiene que evaluar por con webclient funciona pero se evalua la posibilidad de adaptarlo a feign tambien

--- src/main/java/com/ms_Catalogo/Catalogo/CatalogoApplication.java
+++ src/main/java/com/ms_Catalogo/Catalogo/CatalogoApplication.java
@@ -3,9 +3,11 @@ package com.ms_Catalogo.Catalogo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class CatalogoApplication {
 
 	public static void main(String[] args) {
