4de2b1361d57a9d9911de8c3a4a491f9b83df51c
goncalderon2 <gon.calderon@duocuc.cl>
2026-06-15 15:22:51 -0400

    Delete Catalogo-test directory

--- Catalogo-test/src/main/java/com/ms_Catalogo/Catalogo/exception/RecursoNoEncontradoException.java
+++
@@ -1,8 +0,0 @@
-package com.ms_Catalogo.Catalogo.exception;
-
-public class RecursoNoEncontradoException  extends RuntimeException{
-    public RecursoNoEncontradoException(String mensaje) {
-        super(mensaje);
-    }
-
-}
--- Catalogo-test/src/main/java/com/ms_Catalogo/Catalogo/exception/StorageException.java
+++
@@ -1,7 +0,0 @@
-package com.ms_Catalogo.Catalogo.exception;
-
-public class StorageException extends RuntimeException{
-    public StorageException(String mensaje) {
-        super(mensaje);
-    }
-}
